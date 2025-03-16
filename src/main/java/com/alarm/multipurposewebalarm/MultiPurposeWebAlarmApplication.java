63c9c7367d996387dddb3964f00317caca15ffbf
coldrice99 <coldrice99@gmail.com>
2025-03-16 17:26:07 +0900

    init commit

---
+++ src/main/java/com/alarm/multipurposewebalarm/MultiPurposeWebAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.alarm.multipurposewebalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPurposeWebAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiPurposeWebAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alarm/multipurposewebalarm/MultiPurposeWebAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alarm.multipurposewebalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPurposeWebAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
